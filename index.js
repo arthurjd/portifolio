8d964854039d1375eda367dceaef3ce2de385429
Grupo GTEZ <arthurjacob.domingues@gmail.com>
2022-11-30 00:25:40 -0300

    adicionei transacao em eth

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//---------Conectar Carteira; Pegar Info-Conta; Pegar Info-Saldo;---------//
+
 document.getElementById('botaometamask').addEventListener('click', event => {
     let account;
     let button = event.target;
@@ -7,10 +9,30 @@ document.getElementById('botaometamask').addEventListener('click', event => {
         button.textContent = account;
 
         ethereum.request ({method: 'eth_getBalance' , params: [account, 'latest']}).then(result => {
+
             console.log(result);
             let wei = parseInt(result,16);
             let balance = wei / (10**18);
             console.log(balance + " ETH");
         });
     });
+});
+
+//----------------------------Popup JavaScript----------------------------//
+
+const popupAparecerBotao = document.querySelector('#enviarcripto')
+const popup = document.querySelector('.popup-fundoescuro')
+
+popupAparecerBotao.addEventListener('click', () => {
+    popup.style.display = 'block'
+});
+
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0]
+    const classNames = ['popup-close', 'popup-fundoescuro']
+    const deveFecharPopup = classNames.some(className => className === classNameOfClickedElement)
+
+    if (deveFecharPopup) {
+        popup.style.display = 'none'
+    }
 });
--- page-about/About.js
+++ page-about/About.js
@@ -1,3 +1,5 @@
+//---------Conectar Carteira; Pegar Info-Conta; Pegar Info-Saldo;---------//
+
 document.getElementById('botaometamask').addEventListener('click', event => {
     let account;
     let button = event.target;
@@ -7,6 +9,7 @@ document.getElementById('botaometamask').addEventListener('click', event => {
         button.textContent = account;
 
         ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] }).then(result => {
+
             console.log(result);
             let wei = parseInt(result, 16);
             let balance = wei / (10 ** 18);
@@ -15,12 +18,80 @@ document.getElementById('botaometamask').addEventListener('click', event => {
     });
 });
 
-/*
-document.getElementById('enviarcripto').addEventListener('click', event => {
-    let transactionParam = {
-        to: '0xce1AcD66c209CB164fB59B05eDC4ED9B7cf7DdfA',
-        from: account,
-        value: AQUI TEM QUE COLOCAR O VALOR EM HEX ENTRE ''
-    };
+
+
+
+
+
+
+
+
+//----------------------------Popup JavaScript----------------------------//
+
+const popupAparecerBotao = document.querySelector('#enviarcripto')
+const popup = document.querySelector('.popup-fundoescuro')
+
+popupAparecerBotao.addEventListener('click', () => {
+    popup.style.display = 'block'
 });
-*/
+
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0]
+    const classNames = ['popup-close', 'popup-fundoescuro']
+    const deveFecharPopup = classNames.some(className => className === classNameOfClickedElement)
+
+    if (deveFecharPopup) {
+        popup.style.display = 'none'
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------Enviando ETH----------------------------//
+
+const sendEthButton = document.querySelector('#botao-send');
+const valor = document.getElementById('value-eth')
+
+let accounts = [0];
+
+ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
+    account = accounts[0];
+    console.log(account);
+
+    valor.addEventListener('change', () => {
+        console.log(valor)
+    })
+
+    //Sending Ethereum to an address
+    sendEthButton.addEventListener('click', () => {
+        ethereum.request({method: 'eth_sendTransaction',params: [{
+                        from: accounts[0],
+                        to: '0xce1AcD66c209CB164fB59B05eDC4ED9B7cf7DdfA',
+                        value: valor[0],
+                        gasPrice: '0x09184e72a000',
+                        gas: '0x2710',
+                    }],
+            })
+            .then((txHash) => console.log(txHash))
+            .catch((error) => console.error);
+    });
+});
+
+ethereumButton.addEventListener('click', () => {
+    getAccount();
+});
+
+async function getAccount() {
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+}
